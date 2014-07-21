0763e01a0ceef0330ea750fb8ce1f21b7e2a2c2b
d4wner <d4wner@gmail.com>
2014-07-21 17:38:41 +0800

    update

--- monitor.py
+++ monitor.py
@@ -1,4 +1,26 @@
 #! /usr/bin/env python
 #coding=utf-8
+
+
+
+def detect_all(host,last,time,protocol):
+	if protocol == "http":
+		http_detect
+
 if __name__ == "__main__":
+	f=open('config','r')
+	for line in f:
+		#print line
+		if "{" in line:
+			continue
+		elif "host" in line:
+			host=line.split("=")[1]
+			#print host
+		elif "last" in line:
+			last=line.split("=")[1]
+		elif "time" in line:
+			time=line.split("=")[1]
+		elif "protocol" in line:
+			protocol=line.split("=")[1]
+		
 
